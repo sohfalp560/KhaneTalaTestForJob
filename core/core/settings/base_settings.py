b5e18aea17f63abcb6232948e4349f0f193614cb
Soheyl fa <11277168+sohfalp560@users.noreply.github.com>
2025-01-26 23:15:17 +0330

    Start Development. django-admin start new with copy of development settings (settings.dev.py) using django-silk as debug assistant.

---
+++ core/core/settings/base_settings.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
+# Applications
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",  # added due to pass some data to more humanized
+    ## the lib apps
+    "django_extensions", # added due to some tools for easier development and plus commands.
+    ## the project base apps
+]
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
+]
+ROOT_URLCONF = "core.urls"
+
+# Templates and files
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "assets"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+# LANGUAGE_CODE = "fa-ir"
+# TIME_ZONE = "Asia/Tehran"
+USE_I18N = False
+USE_TZ = True
+USE_THOUSAND_SEPARATOR = True
---
+++ core/core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base_settings import *
+
+
+DEBUG = True
+SECRET_KEY = "environ.get('SECRET_KEY')"  # this line should be changed on production
+WSGI_APPLICATION = "core.wsgi.application"
+WEBSITE_URL = "localhost"
+
+# Applications
+INSTALLED_APPS.append('silk.middleware.SilkyMiddleware')
+MIDDLEWARE.append('silk.middleware.SilkyMiddleware')
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "djangoPreBuild.sqlite3",
+    }
+}
