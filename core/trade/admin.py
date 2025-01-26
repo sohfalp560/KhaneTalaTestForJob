37ccac7c41be3a8846be49b6c6e22f965f710962
Soheyl fa <11277168+sohfalp560@users.noreply.github.com>
2025-01-27 00:34:01 +0330

    ruff format

--- core/trade/admin.py
+++ core/trade/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import User, Transaction
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username","balance_rial","balance_gold")
+    list_display = ("username", "balance_rial", "balance_gold")
+
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("user","id","status","price_per_gram")
-    readonly_fields = ("gold_weight_gram","date")
-
+    list_display = ("user", "id", "status", "price_per_gram")
+    readonly_fields = ("gold_weight_gram", "date")
--- core/trade/models.py
+++ core/trade/models.py
@@ -14,15 +14,37 @@ class Transaction(models.Model):
         PENDING = "pn", "on Pending"
         COMPLETED = "cm", "Completed!"
         ERROR = "er", "Error!"
+
     class TypeOfTX(models.TextChoices):
         BUY = "B", "Buy Transaction"
         SELL = "S", "Buy Transaction"
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    type = models.CharField(max_length=1,choices=TypeOfTX,null=False,blank=False,)
-    amount_rial = models.PositiveIntegerField(null=False,blank=False,)
-    gold_weight_gram = models.DecimalField(max_digits=9,decimal_places=3,null=False,blank=False,)
-    price_per_gram = models.PositiveIntegerField(null=False,blank=False,)
-    status = models.CharField(max_length=2,choices=Status,default=Status.PENDING,)
+    type = models.CharField(
+        max_length=1,
+        choices=TypeOfTX,
+        null=False,
+        blank=False,
+    )
+    amount_rial = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+    )
+    gold_weight_gram = models.DecimalField(
+        max_digits=9,
+        decimal_places=3,
+        null=False,
+        blank=False,
+    )
+    price_per_gram = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.PENDING,
+    )
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
