d1c555df92c38b7d6996fde55b9906e19759485f
Soheyl fa <11277168+sohfalp560@users.noreply.github.com>
2025-01-27 00:03:29 +0330

    Transaction model is written! admin registration is added too for both models.

--- core/trade/admin.py
+++ core/trade/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Transaction
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username","balance_rial","balance_gold")
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("user","id","status","price_per_gram")
+    readonly_fields = ("gold_weight_gram","date")
+
--- core/trade/models.py
+++ core/trade/models.py
@@ -7,3 +7,23 @@ class User(AbstractUser):
         default=1000000,
     )
     balance_gold = models.DecimalField(max_digits=12, decimal_places=3, default=0.0)
+
+
+class Transaction(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "pn", "on Pending"
+        COMPLETED = "cm", "Completed!"
+        ERROR = "er", "Error!"
+    class TypeOfTX(models.TextChoices):
+        BUY = "B", "Buy Transaction"
+        SELL = "S", "Buy Transaction"
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=1,choices=TypeOfTX,null=False,blank=False,)
+    amount_rial = models.PositiveIntegerField(null=False,blank=False,)
+    gold_weight_gram = models.DecimalField(max_digits=9,decimal_places=3,null=False,blank=False,)
+    price_per_gram = models.PositiveIntegerField(null=False,blank=False,)
+    status = models.CharField(max_length=2,choices=Status,default=Status.PENDING,)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transaction {self.id} by User {self.user_id}"
